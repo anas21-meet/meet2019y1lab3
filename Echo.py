17f5eb351c7bd0a6e29d851a6c9cb56c4752a253
anas21-meet <anas21@meet.mit.edu>
2019-07-23 05:25:25 -0400

    Stopping here

---
+++ Echo.py
@@ -0,0 +1,5 @@
+user=input("say something!")
+print("hello! echo! can you hear me???".upper())
+print("HELLO! ECHO! CAN YOU HEAR ME???".lower())
+print("hELLO! ECHO! CAN YOU HEAR ME???".swapcase())
+
